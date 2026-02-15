b288c1f251e6b0ed44a44b57a3bc83d85248a394
isnikx0 <isaievnikitaa@gmail.com>
2026-02-15 14:25:38 +0100

    react bug fixing

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { supabase } from "../lib/supabaseClient";
+import React from "react";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
